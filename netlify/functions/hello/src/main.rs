5aa3dd80042b6b36046fed7f24d2e2b224c9f3ef
Jaska Pääkkönen <jaska.paakkonen@gmail.com>
2024-11-27 18:43:31 +0200

    testing

--- netlify/functions/hello/src/main.rs
+++ netlify/functions/hello/src/main.rs
@@ -21,7 +21,7 @@ pub(crate) async fn my_handler(event: ApiGatewayProxyRequest, _ctx: Context) -> 
         status_code: 200,
         headers: HeaderMap::new(),
         multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(format!("Hello from '{}'", path))),
+        body: Some(Body::Text(format!("Terve vaan taas '{}'", path))),
         is_base64_encoded: Some(false),
     };
 
